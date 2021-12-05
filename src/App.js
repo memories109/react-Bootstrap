759d987cab2426b49933e39e8ee3b6db3b6c48a0
Ghosts <memories109@gmail.com>
2021-12-05 10:40:27 +0900

    글제목 list add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [글제목 , 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','강남 헬스장']); 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <div className="list">
+      <h4> {글제목[0]}</h4>
+      <p>2월 18일 발행</p>
+      <hr/>
+      </div>
+      <div className="list">
+      <h4> {글제목[1]}</h4>
+      <p>2월 18일 발행</p>
+      <hr/>
+      </div>
+      <div className="list">
+      <h4> {글제목[2]}</h4>
+      <p>2월 18일 발행</p>
+      <hr/>
+      </div>
+      
+        
     </div>
   );
 }
