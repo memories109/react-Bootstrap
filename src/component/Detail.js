217653d8471e6f34494a9f39db8b730f697ed20a
Ghosts <memories109@gmail.com>
2021-12-14 21:46:42 +0900

    param or detail seq mapping

--- src/component/Detail.js
+++ src/component/Detail.js
@@ -6,17 +6,22 @@ function ModalDetail(props) {
 
     let {id} = useParams();
     let history = useHistory();
+    let detailNum = props.data.find( (a)=> {
+      return a.id == id;
+    });
+    
 
     return (
       <div className="container">
         <div className="row">
           <div className="col-md-6">
-            <img src={'https://codingapple1.github.io/shop/shoes'+id+'.jpg'} width="100%"/>
+            <img src={'https://codingapple1.github.io/shop/shoes'+(detailNum.id+1)+'.jpg'} width="100%"/>
           </div>
           <div className="col-md-6 mt-4"> 
-          <h4 className="pt-5">{props.data[id].title}</h4>
-          <p>{props.data[id].content}</p>
-          <p>{props.data[id].price}</p>
+          
+          <h4 className="pt-5">{detailNum.title}</h4>
+          <p>{detailNum.content}</p>
+          <p>{detailNum.price}</p>
           <button className="btn btn-danger">주문하기</button>
           <button className="btn btn-danger" onClick={ ()=> {
               history.goBack()
